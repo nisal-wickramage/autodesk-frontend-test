f9545b21158cdca12ee574dfa2c9cbaaf95161ea
theNcoder <nisalwickramage@gmail.com>
2020-06-02 19:36:49 +0800

    Moved js code to login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,109 +1,232 @@
-function UserService()
-{
+function User(username, password) {
+    this.Username = username;
+    this.Password = password;
+}
+
+function ValidationResult(errorMessages) {
+    this.ErrorMessages = errorMessages
+}
+
+function ErrorMessage(inputFieldName, message) {
+    this.InputFieldName = inputFieldName;
+    this.Message = message;
+}
+
+function UserService() {
+    this.currentUser = "";
     this.users = [new User("bob", "bob"), new User("sam", "sam"), new User("john", "john")];
 
-    this.ValidateUsername = function(username)
-    {
-        var isValidUsername = this.users.find((user)=> 
-        { 
+    this.ValidateUsername = function (username) {
+        var isValidUsername = this.users.find((user) => {
             return user.Username === username;
         });
-        if(isValidUsername)
-        {
+        if (isValidUsername) {
             return new ValidationResult([]);
         }
-        else
-        {
+        else {
             return new ValidationResult([
                 new ErrorMessage("username", "The username is not recognized.")
             ]);
         }
     }
-    
-    this.ValidatePassword = function(username, password)
-    {
-        var isValidPassword = this.users.find((user)=> 
-        { 
+
+    this.ValidatePassword = function (username, password) {
+        var isValidPassword = this.users.find((user) => {
             return user.Username === username && user.Password === password;
         });
 
-        if(isValidUsername)
-        {
+        if (isValidPassword) {
             return new ValidationResult([]);
         }
-        else
-        {
+        else {
             return new ValidationResult([
                 new ErrorMessage("password", "The password is not recognized.")
             ]);
         }
     }
 
-    this.ValidateUserDetails = function(
-        firstname, 
-        lastname, 
-        username, 
-        confirmUsername, 
-        password, 
-        confirmPassword)
-    {
-        errorMessages = [];
+    this.ValidateUserDetails = function (
+        firstname,
+        lastname,
+        username,
+        confirmUsername,
+        password,
+        confirmPassword) {
+        var errorMessages = [];
 
-        if(firstname)
-        {
-            errorMessages.push(new ErrorMessage("firstname", "Please enter your firstname"));
+        if (!firstname) {
+            errorMessages.push(new ErrorMessage("signupfirstname", "Please enter your firstname"));
         }
-        if(lastname)
-        {
-            errorMessages.push(new ErrorMessage("lastname", "Please enter your lastname"));
+        if (!lastname) {
+            errorMessages.push(new ErrorMessage("signuplastname", "Please enter your lastname"));
         }
-        if(username)
-        {
-            errorMessages.push(new ErrorMessage("username", "Please enter your username"));
+        if (!username) {
+            errorMessages.push(new ErrorMessage("signupusername", "Please enter your username"));
         }
-        if(confirmUsername)
-        {
-            errorMessages.push(new ErrorMessage("confirmUsername", "Please re-enter your username"));
+        if (!confirmUsername) {
+            errorMessages.push(new ErrorMessage("signupretypeusername", "Please re-enter your username"));
         }
-        else if(username === confirmUsername)
-        {
-            errorMessages.push(new ErrorMessage("confirmUsername", "Please enter the same username"));
+        else if (username !== confirmUsername) {
+            errorMessages.push(new ErrorMessage("signupretypeusername", "Please enter the same username"));
         }
-        if(password)
-        {
-            errorMessages.push(new ErrorMessage("password", "Please enter your password"));
+        if (!password) {
+            errorMessages.push(new ErrorMessage("signuppassword", "Please enter your password"));
         }
-        if(confirmPassword)
-        {
-            errorMessages.push(new ErrorMessage("confirmPassword", "Please re-enter your password"));
+        if (!confirmPassword) {
+            errorMessages.push(new ErrorMessage("signupretypepassword", "Please re-enter your password"));
         }
-        else if(password === confirmPassword)
-        {
-            errorMessages.push(new ErrorMessage("confirmPassword", "Please enter the same password"));
+        else if (password !== confirmPassword) {
+            errorMessages.push(new ErrorMessage("signupretypepassword", "Please enter the same password"));
         }
+        return new ValidationResult(errorMessages);
     }
 
-    this.AddUser = function(username, password)
-    {
+    this.AddUser = function (username, password) {
         this.users.push(new User(username, password))
     }
-
-
 }
 
-function User(username, password)
+var userService = new UserService();
+
+function gotoPrevScreen() {
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('pause');
+}
+
+function gotoNextScreen() {
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('pause');
+}
+
+function elementById(id) {
+    return document.getElementById(id);
+}
+
+function elementsByClass(classname) {
+    return document.getElementsByClassName(classname);
+}
+
+function hideErrorMessage(element)
 {
-    this.Username = username;
-    this.Password = password;
+    element.classList.remove("is-invalid");
 }
 
-function ValidationResult(errorMessages)
+function showErrorMessage(element)
 {
-    this.ErrorMessages = errorMessages
+    element.classList.add("is-invalid");            
 }
 
-function ErrorMessage(inputFieldName, message)
+
+function validateUsername()
 {
-    this.InputFieldName = inputFieldName;
-    this.Message = message;
+    var usernameElement = elementById("username");
+    hideErrorMessage(usernameElement);            
+    var username = usernameElement.value;
+
+    var validationResult = userService.ValidateUsername(username);
+
+    if(validationResult.ErrorMessages.length > 0)
+    {
+        var userNameErrorElement = usernameElement.nextElementSibling;
+        userNameErrorElement.innerText = validationResult.ErrorMessages[0].Message;
+        showErrorMessage(usernameElement);  
+    }
+    else
+    {
+        var button = elementById("nextButton");
+        button.innerText = "Verifying"; 
+        button.disabled = true; 
+        userService.currentUser = username;
+        setTimeout(() => {  
+            var button = elementById("nextButton");
+            button.disabled = false; 
+            button.innerText = "Next"; 
+            elementById("passwordpageUsername").innerText = username;
+            elementById("username").value = "";
+            gotoNextScreen();
+        }, 2000);
+    }
 }
+
+function validatePassword()
+{
+    var passwordElement = elementById("password");
+    hideErrorMessage(passwordElement);            
+    var password = passwordElement.value;
+
+    var validationResult = userService.ValidatePassword(userService.currentUser, password);
+
+    if(validationResult.ErrorMessages.length > 0)
+    {
+        var passwordErrorElement = passwordElement.nextElementSibling;
+        passwordErrorElement.innerText = validationResult.ErrorMessages[0].Message;
+        showErrorMessage(passwordElement);  
+    }
+    else
+    {
+        var button = elementById("signinButton");
+        button.innerText = "Verifying"; 
+        button.disabled = true; 
+        elementById("profileUsername").innerHTML = userService.currentUser;
+        setTimeout(() => {  
+            var button = elementById("signinButton");
+            button.disabled = false; 
+            button.innerText = "Sign in"; 
+            elementById("password").value = "";
+            gotoNextScreen();
+        }, 2000);
+    }
+}
+
+function validateUserDetails()
+{
+    var firstnameElement = elementById("signupfirstname");
+    var lastnameElement = elementById("signuplastname");
+    var usernameElement = elementById("signupusername");
+    var retypeusernameElement = elementById("signupretypeusername");
+    var passwordElement = elementById("signuppassword");
+    var retypepasswordElement = elementById("signupretypepassword");
+
+    hideErrorMessage(firstnameElement);
+    hideErrorMessage(lastnameElement);
+    hideErrorMessage(usernameElement);
+    hideErrorMessage(retypeusernameElement);
+    hideErrorMessage(passwordElement);
+    hideErrorMessage(retypepasswordElement);
+
+    var validationResult = userService.ValidateUserDetails(
+        firstnameElement.value,
+        lastnameElement.value,
+        usernameElement.value,
+        retypeusernameElement.value,
+        passwordElement.value,
+        retypepasswordElement.value
+    );
+
+    if(validationResult.ErrorMessages.length > 0)
+    {
+        validationResult.ErrorMessages.forEach(message => {
+            var element = elementById(message.InputFieldName);
+            showErrorMessage(element);
+            element.nextElementSibling.innerHTML = message.Message;
+        });
+    }
+    else
+    {
+        userService.AddUser(usernameElement.value, passwordElement.value);
+
+        firstnameElement.value = "";
+        lastnameElement.value = "";
+        usernameElement.value = "";
+        retypeusernameElement.value = "";
+        passwordElement.value = "";
+        retypepasswordElement.value = "";
+
+        gotoNextScreen();
+        $('#toast').toast('show');
+    }
+}
+//event binding
+elementById("nextButton").addEventListener("click", validateUsername, false);
+elementById("signinButton").addEventListener("click", validatePassword, false);
+elementById("createaccountButton").addEventListener("click", validateUserDetails, false);
